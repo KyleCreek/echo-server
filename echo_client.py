8aa1874f8ca2025484f8ffef5ac085041d1cc615
Kyle Creek <Kyle.A.Creek@gmail.com>
2019-06-28 18:01:46 -0700

    Adding the initial attempt at the HW 2 Files before I break them trying to add additional functionality. Files pass the tests outlined in the "test.py" files.

--- echo_client.py
+++ echo_client.py
@@ -7,10 +7,11 @@ def client(msg, log_buffer=sys.stderr):
     server_address = ('localhost', 10000)
     # TODO: Replace the following line with your code which will instantiate
     #       a TCP socket with IPv4 Addressing, call the socket you make 'sock'
-    sock = None
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
     print('connecting to {0} port {1}'.format(*server_address), file=log_buffer)
     # TODO: connect your socket to the server here.
 
+    sock.connect(server_address)
     # you can use this variable to accumulate the entire message received back
     # from the server
     received_message = ''
@@ -20,6 +21,9 @@ def client(msg, log_buffer=sys.stderr):
     try:
         print('sending "{0}"'.format(msg), file=log_buffer)
         # TODO: send your message to the server here.
+        
+        msg = msg.encode('utf8')
+        sock.sendall(msg)
 
         # TODO: the server should be sending you back your message as a series
         #       of 16-byte chunks. Accumulate the chunks you get to build the
@@ -27,9 +31,27 @@ def client(msg, log_buffer=sys.stderr):
         #       the entire message and then you can break the loop.
         #
         #       Log each chunk you receive.  Use the print statement below to
-        #       do it. This will help in debugging problems
+        #       do it. This will help in debugging problems.
+        
+        # Define an empty string to store the data in
         chunk = ''
-        print('received "{0}"'.format(chunk.decode('utf8')), file=log_buffer)
+
+        # Counter for the amount of data in 
+        data_in = 0
+
+        # Given this is an echo server, we expected to recieve the same 
+        # amount data sent, back
+        data_expect = len(msg)
+
+        # While loop to control data, as it is only sent in 16 bit chunks
+        while data_in < data_expect:
+            data = sock.recv(16)
+            data_in += len(data)
+            chunk += data.decode()
+        
+        # Print the echoed method back to the screen.
+        print('received "{0}"'.format(chunk), file=log_buffer)
+    
     except Exception as e:
         traceback.print_exc()
         sys.exit(1)
@@ -37,6 +59,8 @@ def client(msg, log_buffer=sys.stderr):
         # TODO: after you break out of the loop receiving echoed chunks from
         #       the server you will want to close your client socket.
         print('closing socket', file=log_buffer)
+        sock.close()
+    return chunk
 
         # TODO: when all is said and done, you should return the entire reply
         # you received from the server as the return value of this function.
--- echo_server.py
+++ echo_server.py
@@ -8,7 +8,11 @@ def server(log_buffer=sys.stderr):
     address = ('127.0.0.1', 10000)
     # TODO: Replace the following line with your code which will instantiate
     #       a TCP socket with IPv4 Addressing, call the socket you make 'sock'
-    sock = None
+
+            # socket.AF_INET tells the program to expect IPv4 Addresses
+            # socket.SOCK_Stream defines the socket type
+            # socket.IPPROTO_TCP defines the protocol number
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
     # TODO: You may find that if you repeatedly run the server script it fails,
     #       claiming that the port is already used.  You can set an option on
     #       your socket that will fix this problem. We DID NOT talk about this
@@ -21,6 +25,12 @@ def server(log_buffer=sys.stderr):
 
     # TODO: bind your new sock 'sock' to the address above and begin to listen
     #       for incoming connections
+    
+    # Bind the socket to the address
+    sock.bind(address)
+
+    # Listen for incoming connections
+    sock.listen(1)
 
     try:
         # the outer loop controls the creation of new connection sockets. The
@@ -33,7 +43,13 @@ def server(log_buffer=sys.stderr):
             #       the client so we can report it below.  Replace the
             #       following line with your code. It is only here to prevent
             #       syntax errors
-            conn, addr = ('foo', ('bar', 'baz'))
+            #conn, addr = ('foo', ('bar', 'baz'))
+
+
+            # 'foo' corresponds to the connection
+            # addr correlates to a tuple, the IP Address and What I assume to be
+            # the port, that is why the tupple contains a tuple
+            conn, addr = sock.accept()
             try:
                 print('connection - {0}:{1}'.format(*addr), file=log_buffer)
 
@@ -46,12 +62,23 @@ def server(log_buffer=sys.stderr):
                     #       following line with your code.  It's only here as
                     #       a placeholder to prevent an error in string
                     #       formatting
-                    data = b''
+                    
+                    # Since this is in the While loop, it will continue to recieve
+                    # data until all the information has been recieved
+                    data = conn.recv(16)
                     print('received "{0}"'.format(data.decode('utf8')))
+
+                    if not data:
+                        break
+
+
                     
                     # TODO: Send the data you received back to the client, log
                     # the fact using the print statement here.  It will help in
                     # debugging problems.
+
+                    conn.sendall(data)
+
                     print('sent "{0}"'.format(data.decode('utf8')))
                     
                     # TODO: Check here to see whether you have received the end
@@ -62,6 +89,8 @@ def server(log_buffer=sys.stderr):
                     # message is a trick we learned in the lesson: if you don't
                     # remember then ask your classmates or instructor for a clue.
                     # :)
+
+
             except Exception as e:
                 traceback.print_exc()
                 sys.exit(1)
@@ -69,6 +98,7 @@ def server(log_buffer=sys.stderr):
                 # TODO: When the inner loop exits, this 'finally' clause will
                 #       be hit. Use that opportunity to close the socket you
                 #       created above when a client connected.
+                conn.close()
                 print(
                     'echo complete, client connection closed', file=log_buffer
                 )
@@ -78,7 +108,7 @@ def server(log_buffer=sys.stderr):
         #       close the server socket and exit from the server function.
         #       Replace the call to `pass` below, which is only there to
         #       prevent syntax problems
-        pass
+        sock.close()
         print('quitting echo server', file=log_buffer)
 
 
